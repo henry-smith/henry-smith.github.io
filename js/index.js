73aed688784f3469f1678c1b2f2ef9329e4faf54
Henry Smith <henry.smith@berkeley.edu>
2017-01-23 10:38:54 -0800

    gonna delete a bunch of experimenting stuff

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
-alert("thiafdlerfdgsdfget");
+window.onload = function () {
+  var header = document.getElementById('header');
+  header.className = "splash";
+  headerChildren = header.childNodes;
+  for (i = 0; i < headerChildren.length; i++) {
+    headerChildren[i].className = 'splash';
+  }
+
+  var tablinks = document.getElementsByClassName('tablinks');
+  for(i = 0; i < tablinks.length; i++) {
+    tablinks[i].addEventListener('onclick', openCity, false);
+  }
+
+  header.onclick = function () {
+    header.className = 'main';
+    for (i = 0; i < headerChildren.length; i++) {
+      headerChildren[i].className = 'main';
+    }
+  }
+
+
+  function changeTab(evt, newTab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    console.log("here")
+    document.getElementById(newTab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+}
